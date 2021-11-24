98d38175a9805f50755c2244569790c641cdbb38
Michael Haymon <mhaymon2010@gmail.com>
2021-11-24 10:58:04 -0600

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+import java.util.List;
+
+public class Main {
+
+    int employeeThreshold = 3;
+    int salaryThreshold = 10000;
+
+
+    public static void main(String[] args) {
+        List<String> stringList = Arrays.asList("Test 1", "Test 2", "Test 3");
+    }
+
+
+}
---
+++ src/Prospect.java
@@ -0,0 +1,9 @@
+public class Prospect {
+    private static final int MINIMUM_AMOUNT_PER_ROLE = 15000;
+    private static final int MAXIMUM_AMOUNT_PER_ROLE = 500000;
+    private static final int MINIMUM_POSITIONS = 2;
+    int contractLengthInMonths;
+    int positions;
+    int bidAmountPerYear;
+    String[] practiceAreas;
+}
