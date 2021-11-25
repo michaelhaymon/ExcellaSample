72ad0f4dd066819de1d26aef76fa72bc88b77451
Michael Haymon <mhaymon2010@gmail.com>
2021-11-24 21:12:12 -0600

    EOD Commit

--- src/Main.java
+++ src/Main.java
@@ -3,12 +3,10 @@ import java.util.List;
 
 public class Main {
 
-    int employeeThreshold = 3;
-    int salaryThreshold = 10000;
-
-
     public static void main(String[] args) {
-        List<String> stringList = Arrays.asList("Test 1", "Test 2", "Test 3");
+//        recieveWebProspect();
+
+//
     }
 
 
--- src/Prospect.java
+++ src/Prospect.java
@@ -1,9 +1,13 @@
+import java.math.BigDecimal;
+import java.util.Optional;
+
 public class Prospect {
     private static final int MINIMUM_AMOUNT_PER_ROLE = 15000;
     private static final int MAXIMUM_AMOUNT_PER_ROLE = 500000;
-    private static final int MINIMUM_POSITIONS = 2;
+    private static final int MINIMUM_POSITIONS = 3;
     int contractLengthInMonths;
     int positions;
-    int bidAmountPerYear;
+    int bidAmount;
     String[] practiceAreas;
+    BigDecimal RelativeValuePerEmployeePerMonth;
 }
